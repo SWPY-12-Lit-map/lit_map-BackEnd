404852faa46690a03c90c302a96d1749c86f83c9
Tedeeee <gksrmf359@gmail.com>
2024-07-01 14:24:12 +0900

    feat : API 문서를 위해 Swagger 도입 #5

--- src/main/java/com/lit_map_BackEnd/domain/board/controller/Controller.java
+++ src/main/java/com/lit_map_BackEnd/domain/board/controller/Controller.java
@@ -6,6 +6,7 @@ import com.lit_map_BackEnd.domain.board.dto.CodeDto;
 import com.lit_map_BackEnd.domain.board.service.CodeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/board")
 @RequiredArgsConstructor
 @Tag(name = "board 관련", description = "작품 등록관련")
 public class Controller {
@@ -28,10 +29,9 @@ public class Controller {
     @PostMapping("")
     @Operation(summary = "작품 등록", description = "작품을 등록할때 사용하는 API")
     @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "1000", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json")),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "2002", description = "이미 가입된 계정입니다.", content = @Content(mediaType = "application/json")),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "4000", description = "데이터베이스 연결에 실패하였습니다.", content = @Content(mediaType = "application/json")),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "4011", description = "비밀번호 암호화에 실패하였습니다.", content = @Content(mediaType = "application/json"))
+            @ApiResponse(responseCode = "201", description = "데이터 삽입 성공", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "데이터 삽입 문제 발생", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "handle Validation Exception -> 유효성 오류", content = @Content(mediaType = "application/json"))
     })
     public ResponseEntity<SuccessResponse> insertCode(@RequestBody @Valid CodeDto codeDto) {
         int N = codeService.insertCode(codeDto);
