89fa25140ea690b59ebb94a255c5fb7585f29ce2
Tedeeee <gksrmf359@gmail.com>
2024-07-05 01:43:18 +0900

    docs : 캐릭터 등록 controller Swagger Api 문서 추가 #9

--- src/main/java/com/lit_map_BackEnd/domain/character/controller/CastController.java
+++ src/main/java/com/lit_map_BackEnd/domain/character/controller/CastController.java
@@ -4,6 +4,7 @@ import com.lit_map_BackEnd.common.exception.code.SuccessCode;
 import com.lit_map_BackEnd.common.exception.response.SuccessResponse;
 import com.lit_map_BackEnd.domain.character.dto.CastRequestDto;
 import com.lit_map_BackEnd.domain.character.service.CastService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,6 +26,7 @@ public class CastController {
     private final CastService castService;
 
     @PostMapping("")
+    @Operation(summary = "해당 작품에 캐릭터 등록", description = "작품에 등록될 캐릭터 등록하기")
     public ResponseEntity<SuccessResponse> insertCharacter(@RequestBody List<@Valid CastRequestDto> castRequestDto) {
         int N = castService.insertCharacter(castRequestDto);
 
