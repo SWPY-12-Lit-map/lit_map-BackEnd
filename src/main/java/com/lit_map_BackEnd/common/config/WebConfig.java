c3ac147bea1d23c646f180d23ce0dea2a2e477de
Tedeeee <gksrmf359@gmail.com>
2024-07-26 17:06:48 +0900

    fix : cors 오류 #32

--- src/main/java/com/lit_map_BackEnd/common/config/WebConfig.java
+++ src/main/java/com/lit_map_BackEnd/common/config/WebConfig.java
@@ -9,10 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://api.litmap.store")
+                .allowedOrigins("https://api.litmap.store", "http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:8080", "http://127.0.0.1:3000", "http://localhost:5173")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+
     }
 }
